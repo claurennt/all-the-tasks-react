2ef5d04ee632a9fb4fe7ecbc0383c8e971d42aa9
Franz <franz.hanschmidt@posteo.de>
2021-05-25 22:35:23 +0200

    copy paste project data

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useState } from "react";
+// import "./App.css";
+import Item from "./Item";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [items, addItem] = useState([
+    {
+      itemId: nanoid(),
+      itemText: "Drink water",
+      itemChecked: false,
+    },
+    {
+      itemId: nanoid(),
+      itemText: "Drink wine",
+      itemChecked: false,
+    },
+  ]);
+
+  const addTask = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main class="list-wrapper">
+      <section class="list">
+        <h1>Remember me :)</h1>
+
+        <div class="popup">
+          <span class="popuptext" id="myPopup">
+            Missing item!
+          </span>
+        </div>
+        <form class="submitForm" onSubmit={addTask}>
+          <div class="input-wrapper">
+            <input type="text" placeholder="Add Item..." class="input-item" />
+            <button id="input-button" class="input-button" type="submit">
+              +
+            </button>
+          </div>
+        </form>
+        <div class="item-wrapper">
+          {items.map((item) => {
+            return <Item key={item.itemId} itemText={item.itemText} />;
+          })}
+        </div>
+        <div class="reset-local-storage">Reset list</div>
+      </section>
+    </main>
   );
 }
 
---
+++ src/Item.js
@@ -0,0 +1,20 @@
+import react from "react";
+
+export default function Item(props) {
+  return (
+    <article class="item">
+      <div class="item-textfield">{props.itemText}</div>
+      <div class="button-wrapper">
+        <button class="edit item-button">
+          <i class="far fa-edit"></i>
+        </button>
+        <button class="check item-button">
+          <i class="far fa-check-square"></i>
+        </button>
+        <button class="delete item-button">
+          <i class="far fa-trash-alt"></i>
+        </button>
+      </div>
+    </article>
+  );
+}
