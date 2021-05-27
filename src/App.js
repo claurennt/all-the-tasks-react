19433f52d5552a52d4cbb744ad721e692497a3fc
claurennt <c.benedetti1989@gmail.com>
2021-05-27 16:36:42 +0200

    clean up project and add comments

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-// import logo from "./logo.svg";
 import React, { useState } from "react";
-// import "./App.css";
+
 import Item from "./Item";
 import { nanoid } from "nanoid";
 import { ToastContainer, toast } from "react-toastify";
@@ -22,6 +21,7 @@ function App() {
     },
   ]);
 
+  // function to add tasks
   const addTask = (e) => {
     e.preventDefault();
     const newItem = {
@@ -31,8 +31,10 @@ function App() {
       editing: false,
     };
     addItem([...items, newItem]);
+    e.currentTarget.newItem.value = "";
   };
 
+  //function to toggle itemChecked property of clicked item based on id
   const toggleChecked = (itemId) => {
     const toggledItems = items.map((item) => {
       if (item.itemId === itemId) {
@@ -44,22 +46,32 @@ function App() {
     addItem(toggledItems);
   };
 
+  // function to delete clicked item
   const deleteItem = (itemId) => {
+    // updates state with all filtered items but the one we are clicking on(identified by id)
     const filteredItems = items.filter((item) => itemId !== item.itemId);
     addItem(filteredItems);
   };
 
+  // function to edit text
   const editItem = (itemId) => {
+    /*shows toast if we click on more than one item
+     (identified by the length of the array that filters the items by their editing property)
+    */
     if (items.filter((i) => i.editing).length) {
       return toast.error("You are already editing a task!", {
         position: toast.POSITION.BOTTOM_RIGHT,
       });
     }
+    // find the item we want to edit when we click on the edit button by the id and make it's editing prop to true
     const targetItem = items.find((i) => i.itemId === itemId);
     targetItem.editing = true;
     addItem([...items.filter((i) => i.itemId !== itemId), targetItem]);
   };
 
+  /*when we finish the editItem(onBlur in item.js), we set the editing property 
+  back to false(this makes the contentEditable prop in the child = to false), 
+  then we subsitute the itemText property of that item with event.target.innerText and reupdate the state*/
   const finishEditItem = (event, itemId) => {
     const targetItem = items.find((i) => i.itemId === itemId);
     targetItem.editing = false;
--- src/Item.js
+++ src/Item.js
@@ -8,12 +8,19 @@ export default function Item({
   editing,
   onFinishEditing,
 }) {
+  // saved the conditional rendering expressions in a var for better redability
+  const checked = itemChecked
+    ? "item-textfield checked-item"
+    : "item-textfield";
+  const edit = editing && "item-visible";
+
   return (
     <article className="item">
       <div
-        className={
-          itemChecked ? "item-textfield checked-item" : "item-textfield"
-        }
+        className={`${checked} ${edit}`}
+        /* if we click the edit button down below, the contentEditable will become true 
+        because inside the editItem changed the value of editing that we pass here from the parent 
+        and we use that property to conditionally change the contentEditable attribute of the div */
         contentEditable={editing ? "true" : "false"}
         suppressContentEditableWarning={true}
         onBlur={(event) => onFinishEditing(event, itemId)}
